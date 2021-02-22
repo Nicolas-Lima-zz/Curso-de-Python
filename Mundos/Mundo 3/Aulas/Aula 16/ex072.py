78afe3d25eb1f6e96d2a753faf21be1e28c848e2
Nícolas de Lima Alves <nicolasdelimaalves00@gmail.com>
2021-02-22 15:33:02 -0300

    exercícios ( Aula 16 )
    Todos os exercícios da aula 16 do curso de python ( Curso em Vídeo )

--- Mundos/Mundo 3/Aulas/Aula 16/ex072.py
+++ Mundos/Mundo 3/Aulas/Aula 16/ex072.py
@@ -1,1 +1,5 @@
-tupla = 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+tupla = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+escolher = int(input('Digite um número entre 0 e 20: '))
+while escolher > 20:
+    escolher = int(input('Digite um número entre 0 e 20: '))
+print(f'Você digitou o número {tupla[escolher]}')
