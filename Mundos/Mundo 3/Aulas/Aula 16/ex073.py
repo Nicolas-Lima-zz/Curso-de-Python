23cad742027046cdf001d7dad6e14e7cdaeba1e0
Nícolas de Lima Alves <nicolasdelimaalves00@gmail.com>
2021-02-25 10:39:31 -0300

    Update ex073.py

--- Mundos/Mundo 3/Aulas/Aula 16/ex073.py
+++ Mundos/Mundo 3/Aulas/Aula 16/ex073.py
@@ -1,12 +1,20 @@
-tupla = 'Flamengo', 'Internacional', 'Atlético-MG', 'São Paulo', 'Fluminense', 'Grêmio', 'Palmeiras', 'Santos', \
+times = 'Flamengo', 'Internacional', 'Atlético-MG', 'São Paulo', 'Fluminense', 'Grêmio', 'Palmeiras', 'Santos', \
         'Athletico-PR', 'Corinthians', 'Bragantino', 'Ceará SC', 'Atlético-GO', 'Sport Recife', 'Bahia', 'Fortaleza', \
-        'Vasco da Gama', 'Goiás', 'Coritiba', 'Botafogo '
-print('-=' * 20)
-print(f'\nOs cinco primeiros colocados são: {tupla[:5]}\n')
-print('-=' * 20)
-print(f'\nOs últimos 4 colocados da tabela {tupla[16:]}\n')
-print('-=' * 20)
-print(f'\nLista dos times em ordem alfabética: {sorted(tupla)}\n')
-print('-=' * 20)
-print('\nO São Paulo está na {}° posição\n'.format(tupla.index('São Paulo') + 1))
-print('-=' * 20)
+        'Vasco da Gama', 'Goiás', 'Coritiba', 'Botafogo'
+print('=' * 10, 'Os 5 primeiros colocados', '=' * 10, '\n')
+for c in range(0, 5):
+    print(f'{c + 1}° {times[c]}')
+print('')
+print('=' * 10, 'Os 4 últimos colocados', '=' * 10)
+print('')
+for c in range(15, 20):
+    print(f'{c + 1}° {times[c]}')
+print('')
+print('=' * 10, 'Times em ordem alfabética', '=' * 10)
+print(f'{sorted(times)}')
+print('')
+print('=' * 10, 'Posição do time São Paulo', '=' * 10)
+print('')
+print(f'O time do São Paulo está na {times.index("São Paulo") + 1}° posição')
+print('')
+print('=' * 51)
