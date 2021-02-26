93602a360ac39c2c7c893d8a94243f3fa07706e0
Nícolas de Lima Alves <nicolasdelimaalves00@gmail.com>
2021-02-26 13:32:41 -0300

    Update ex075.py

--- Mundos/Mundo 3/Aulas/Aula 16/ex075.py
+++ Mundos/Mundo 3/Aulas/Aula 16/ex075.py
@@ -1,8 +1,21 @@
 lista = []
-num = 0
+par = []
+num = cont = merda = a = 0
 for c in range(0, 4):
     num = int(input('Digite um número: '))
+    if num % 2 == 0:
+        par.append(num)
+        cont += 1
+    if num == num:
+        num = num
     lista.append(num)
-    tuple(lista)
+print(f'Você digitou os valores {tuple(lista)}')
 print(f'O número 9 apareceu {lista.count(9)} vezes')
-print(lista)
+if 3 in lista:
+    a = f'O número 3 apareceu na {lista.index(3) + 1}° posição'
+else:
+    a = 'O número 3 não foi digitado'
+print(a)# se o 3 foi digitado ou não
+print('Números pares:', end=' ')
+for c in range(0, cont):
+    print(f'{par[c]}', end=' ')
