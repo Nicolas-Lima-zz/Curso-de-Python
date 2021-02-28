8142591c72af12a95f113ac5a9f42ea0ce8f0eaf
Nícolas de Lima Alves <nicolasdelimaalves00@gmail.com>
2021-02-28 16:40:53 -0300

    Update ex077.py

--- Mundos/Mundo 3/Aulas/Aula 16/ex077.py
+++ Mundos/Mundo 3/Aulas/Aula 16/ex077.py
@@ -1,7 +1,12 @@
-tupla = ('Morango', 'Jabuticaba', 'Casa', 'Carro', 'Zebra', 'Tigre', 'Peixe')
-codigo = '\n'
-for palavra in tupla:
-    print(f'\nNa palavra {palavra.upper()} temos', end=' ')
+lista = []
+while True:
+    palavras = str(input('Digite a palavra: '))
+    continuar = str(input('Quer adicionar mais palavras? [S/N] ')).upper()[0]
+    lista.append(palavras)
+    if continuar == 'N':
+        break
+for palavra in lista:
+    print(f'\nNa palavra {palavra} temos', end=' ')
     for letra in palavra:
         if letra in 'aeiou':
             print(letra, end=' ')
