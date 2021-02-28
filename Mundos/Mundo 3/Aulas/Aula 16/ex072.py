16e84111eddbcd55f4c50b2ae038985313efcfe8
Nícolas de Lima Alves <nicolasdelimaalves00@gmail.com>
2021-02-28 15:44:22 -0300

    Update ex072.py

--- Mundos/Mundo 3/Aulas/Aula 16/ex072.py
+++ Mundos/Mundo 3/Aulas/Aula 16/ex072.py
@@ -2,3 +2,4 @@ tupla = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito'
 escolher = int(input('Digite um número entre 0 e 20: '))
 while escolher > 20:
     escolher = int(input('Digite um número entre 0 e 20: '))
+print(f'Você digitou o número {tupla[escolher]}')
