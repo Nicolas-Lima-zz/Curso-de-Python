aa20ba574db7dd4d86c654a4b24cd99adf98f162
Nícolas de Lima Alves <nicolasdelimaalves00@gmail.com>
2021-02-28 16:48:27 -0300

    Update ex072.py

--- Mundos/Mundo 3/Aulas/Aula 16/ex072.py
+++ Mundos/Mundo 3/Aulas/Aula 16/ex072.py
@@ -1,4 +1,5 @@
-tupla = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+tupla = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze'\
+    , 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
 escolher = int(input('Digite um número entre 0 e 20: '))
 while escolher > 20:
     escolher = int(input('Digite um número entre 0 e 20: '))
