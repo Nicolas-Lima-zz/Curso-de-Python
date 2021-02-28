4e5b3e95b651d785d97b30dff40538ceeab788b0
Nícolas de Lima Alves <nicolasdelimaalves00@gmail.com>
2021-02-28 17:11:07 -0300

    Update ex072.py

--- Mundos/Mundo 3/Aulas/Aula 16/ex072.py
+++ Mundos/Mundo 3/Aulas/Aula 16/ex072.py
@@ -1,6 +1,12 @@
-tupla = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze'\
+tupla = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze' \
     , 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
-escolher = int(input('Digite um número entre 0 e 20: '))
-while escolher > 20:
+while True:
     escolher = int(input('Digite um número entre 0 e 20: '))
-print(f'Você digitou o número {tupla[escolher]}')
+    while escolher > 20 or escolher <= -1:
+        escolher = int(input('Digite um número entre 0 e 20: '))
+    print(f'Você digitou o número {tupla[escolher]}')
+    continuar = str(input('Quer continuar: [S/N] ')).upper()
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar: [S/N] '))
+    if continuar == 'N':
+        break
